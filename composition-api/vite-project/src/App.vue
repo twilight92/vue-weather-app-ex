3ba6198d0b10f543c4779dff2af9a0cc42a4b3b7
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 22:37:45 +0900

    자식 컴포넌트 불러오기

--- composition-api/vite-project/src/App.vue
+++ composition-api/vite-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // props 변수(상태변수) 추가시 ref 함수 필요
 import { onMounted, ref } from "vue";
+import ChildCompVue from "./components/ChildComp.vue";
 
 let count = ref(0); // 상태변수 초기값(state)
 let title = "Hello Vue";
---
+++ composition-api/vite-project/src/components/ChildComp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
