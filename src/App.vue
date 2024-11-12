0d4f41dbfb887669f365bff9a0c01920fa77d33c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 19:45:57 +0900

    코드 정리

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,32 @@
 <script setup>
-  import Navbar from './components/Navbar.vue';
-  import MainComp from './components/MainComp.vue';
-  import About from './components/About.vue';
-  import { ref, onMounted } from 'vue';
-  import { useStore } from 'vuex';
- 
-  const store = useStore();
+import Navbar from "./components/Navbar.vue";
+import MainComp from "./components/MainComp.vue";
+import About from "./components/About.vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
 
-  // 앱이 실행되면 날씨 데이터 가져오기
-  onMounted(() => {
-    store.dispatch('getWeather');
-  })
+const store = useStore();
 
-  const onSearchCity = (city) => {
-    weatherData.value.city = city;
-    getWeather();
-  }
+// 앱이 실행되면 날씨 데이터 가져오기
+onMounted(() => {
+  store.dispatch("getWeather");
+});
+
+const onSearchCity = (city) => {
+  weatherData.value.city = city;
+  getWeather();
+};
 </script>
 
 <template>
   <!-- <button @click="$store.dispatch('getWeather')">getWeather</button> -->
-  <Navbar/>
+  <Navbar />
   <div v-if="!$store.state.toggle">
-    <MainComp/>
+    <MainComp />
   </div>
   <div v-else>
-    <About/>
+    <About />
   </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
