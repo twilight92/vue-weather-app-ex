4a029002ea36a20fd6b2b07f03d7c1af05d9f276
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 22:37:36 +0900

    composition api 적용

--- composition-api/vite-project/src/App.vue
+++ composition-api/vite-project/src/App.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+// props 변수(상태변수) 추가시 ref 함수 필요
+import { onMounted, ref } from "vue";
 
-<template></template>
+let count = ref(0); // 상태변수 초기값(state)
+let title = "Hello Vue";
+
+onMounted(() => {
+  console.log("mounted");
+});
+
+const increaseCount = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increaseCount">count++</button>
+    <p>{{ count }}</p>
+    <ChildCompVue />
+  </div>
+</template>
 
 <style scoped></style>
