a8c370f7c70c4c31001ab22ace1aa7bdf40e0a04
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 22:04:28 +0900

    코드정리

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,29 @@
 <template>
   <nav class="navbar">
     <button class="toggle" @click="$store.commit('toggleButton')">
-      <font-awesome-icon class="icon" :icon="['fas', 'bars-staggered']"/>
+      <font-awesome-icon class="icon" :icon="['fas', 'bars-staggered']" />
     </button>
   </nav>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
-  .navbar {
-    background: #fff;
-    width: 100%;
-    padding: 20px;
-    display: flex;
-    justify-content: end;
+.navbar {
+  background: #fff;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: end;
 
-    button {
-      border: none;
-      background: transparent;
-      cursor: pointer;
-    }
-    .icon {
-      color: #333;
-      font-size: 24px;
-    }
+  button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
   }
+  .icon {
+    color: #333;
+    font-size: 24px;
+  }
+}
 </style>
-
