107523a740e14e175bd66be54c87bfd5fb102c94
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 22:02:03 +0900

    WeatherInfo에 pinia store 적용

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="weather-info">
-    <!-- <p>{{ $store.state.weatherData.icon }}</p> -->
+    <!-- <p>{{ weatherData.icon }}</p> -->
     <div class="icon">
       <img
-        :src="`https://openweathermap.org/img/wn/${$store.state.weatherData.icon}@2x.png`"
-        :alt="$store.state.weatherData.icon"
+        :src="`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`"
+        :alt="weatherData.icon"
       />
     </div>
-    <div class="temp">
-      {{ ($store.state.weatherData.temp - 273.15).toFixed(1) }}&deg;
-    </div>
-    <div class="text">{{ $store.state.weatherData.text }}</div>
+    <div class="temp">{{ (weatherData.temp - 273.15).toFixed(1) }}&deg;</div>
+    <div class="text">{{ weatherData.text }}</div>
     <div class="location">
-      {{ $store.state.weatherData.city }},
-      {{ $store.state.weatherData.location }}
+      {{ weatherData.city }},
+      {{ weatherData.location }}
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from "../store/store";
+import { storeToRefs } from "pinia";
+
+// store 가져오기
+const store = useStore();
+const { weatherData } = storeToRefs(store);
+</script>
 
 <style lang="scss" scoped>
 .weather-info {
