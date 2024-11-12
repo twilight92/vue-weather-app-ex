e56e8c20e821275f01304d601f0c3fde226bbad7
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 19:21:54 +0900

    코드 정렬

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import store from './store/store'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // 사용할 아이콘 불러오기
-import { faBarsStaggered, faLocationDot, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBarsStaggered,
+  faLocationDot,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 // 아이콘 등록하기
 library.add(faBarsStaggered, faLocationDot, faMagnifyingGlass);
 
 createApp(App)
   .use(store)
-  .component('font-awesome-icon', FontAwesomeIcon)
-  .mount('#app')
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
