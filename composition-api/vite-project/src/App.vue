ac2faea4ae22ee8000a8ac4690cba148e85f8f05
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-11-12 22:47:12 +0900

    composition api 실습

--- composition-api/vite-project/src/App.vue
+++ composition-api/vite-project/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 // props 변수(상태변수) 추가시 ref 함수 필요
-import { onMounted, ref } from "vue";
+import { ref, onMounted, watch } from "vue";
 import ChildCompVue from "./components/ChildComp.vue";
 
 let count = ref(0); // 상태변수 초기값(state)
 let title = "Hello Vue";
+let color = ref("green");
 
 onMounted(() => {
   console.log("mounted");
@@ -13,6 +14,10 @@ onMounted(() => {
 const increaseCount = () => {
   count.value++;
 };
+
+watch(color, () => {
+  console.log("color 변경");
+});
 </script>
 
 <template>
@@ -20,7 +25,8 @@ const increaseCount = () => {
     <h1>{{ title }}</h1>
     <button @click="increaseCount">count++</button>
     <p>{{ count }}</p>
-    <ChildCompVue />
+    <ChildCompVue :color="color" bgColor="yellow" />
+    <button @click="color = 'red'">Change color</button>
   </div>
 </template>
 
--- composition-api/vite-project/src/components/ChildComp.vue
+++ composition-api/vite-project/src/components/ChildComp.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <h2>Child Component</h2>
+    <!-- style 안에 들어가는 내용은 자료형이기 때문에 객체 형태로 입력 -->
+    <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  color: String,
+  bgColor: String,
+});
+
+console.log(props.color);
+</script>
 
 <style></style>
